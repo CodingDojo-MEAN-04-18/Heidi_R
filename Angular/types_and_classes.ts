3da20a636f10cb214630ad9cef06a893fa7f9f0d
Heidi <hlreidwins@gmail.com>
2018-05-08 12:34:08 -0700

    types and classes updated

--- Angular/types_and_classes.ts
+++ Angular/types_and_classes.ts
@@ -1,28 +1,25 @@
 var myNum: number = 5;
 let myString: string = "Hello Universe";
 var myArr: Array<number> = [1,2,3,4];
-class myObj {[key: string]: (string | number)} = {name: 'Bill'};
+let myObj: {[key: string]: (string | number)} = {name: 'Bill'};
 var anythingVariable: any = "Hey";
 anythingVariable = 25; 
-var arrayOne: Array <boolean> = [true, false, true, true]; 
-var arrayTwo: any =  [1, 'abc', true, 2];
-let newobj = new myObj({ x: 5, y: 10 });
+const arrayOne: boolean[] = [true, false, true, true]; 
+const arrayTwo: (number|string|boolean)[] = [1,'abc',true,2];
+myObj = { x: 5, y: 10 };
 class MyNode{
-    constructor(val: Number){
-        this.val = 0;
-        this.val = val;
-    }
+    private _priv: number;
+    constructor(public val: Number){}
     doSomething(){
         this._priv = 10
     }
-    return MyNode;
-}());
-myNodeInstance = new MyNode(1);
+};
+const myNodeInstance: MyNode = new MyNode(1)
 console.log(myNodeInstance.val);
-function myFunction() {
+function myFunction(): void{
     console.log("Hello World");
     return;
 }
-function sendingErrors() {
+function sendingErrors(): never {
 	throw new Error('Error message');
 }
