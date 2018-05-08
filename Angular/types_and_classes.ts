9a122ab8a7ce8c056cfdc14d42288532bc3b6dd9
Heidi <hlreidwins@gmail.com>
2018-05-08 12:33:21 -0700

    types and classes updated

--- Angular/types_and_classes.ts
+++ Angular/types_and_classes.ts
@@ -1,21 +1,20 @@
-var myNum: Number = 5;
-let myString: String = "Hello Universe";
+var myNum: number = 5;
+let myString: string = "Hello Universe";
 var myArr: Array<number> = [1,2,3,4];
-class myObj { name:'Bill'};
+class myObj {[key: string]: (string | number)} = {name: 'Bill'};
 var anythingVariable: any = "Hey";
 anythingVariable = 25; 
 var arrayOne: Array <boolean> = [true, false, true, true]; 
 var arrayTwo: any =  [1, 'abc', true, 2];
-myObj = { x: 5, y: 10 };
-// object constructor
-MyNode = (function () {
-    function MyNode(val) {
+let newobj = new myObj({ x: 5, y: 10 });
+class MyNode{
+    constructor(val: Number){
         this.val = 0;
         this.val = val;
     }
-    MyNode.prototype.doSomething = function () {
-        this._priv = 10;
-    };
+    doSomething(){
+        this._priv = 10
+    }
     return MyNode;
 }());
 myNodeInstance = new MyNode(1);
