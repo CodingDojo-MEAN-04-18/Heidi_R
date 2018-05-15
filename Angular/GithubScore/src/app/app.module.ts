44d2737ce18b6f938395920710efdadfb075d8de
Heidi <hlreidwins@gmail.com>
2018-05-15 13:24:20 -0700

    GitHub Score

---
+++ Angular/GithubScore/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { DataService } from './data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+  providers: [
+    DataService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
